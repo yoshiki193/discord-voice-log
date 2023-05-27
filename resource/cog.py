450b9c65e6b59acc08e65d5464aa9c1665ff55b4
maromu193 <maromu193@gmail.com>
2023-05-27 07:53:51 +0000

    "master"

--- resource/cog.py
+++ resource/cog.py
@@ -8,21 +8,12 @@ class command(commands.Cog):
         self.bot=bot
         self.logch=None
     
-    @app_commands.command(name="select_logch",description="select text channel for logging")
-    async def select_logch(self,interaction:discord.Interaction,channel:discord.TextChannel=None):
-        self.logch=channel
-        if self.logch is None:
-            await interaction.response.send_message(f"log system's message channel")
-        else:
-            await interaction.response.send_message(f"log {self.logch}")
-
     @commands.Cog.listener()
     async def on_voice_state_update(self,member,before,after):
         if after.channel!=before.channel:
             if before.channel!=None:
-                sendch=member.guild.system_channel if self.logch is None else self.logch
+                sendch=member.guild.system_channel
                 if len(before.channel.members)==0:
-                    self.timee=datetime.datetime.now().replace(microsecond=0)
                     data={
                         "title":"Initiating call",
                         "fields":[
@@ -35,19 +26,13 @@ class command(commands.Cog):
                                 "name":"By",
                                 "value":f"{member}",
                                 "inline":True
-                            },
-                            {
-                                "name":"Time",
-                                "value":f"{self.timee-self.times}",
-                                "inline":True
                             }
                         ]
                     }
                     await sendch.send(embed=discord.Embed.from_dict(data=data))
             if after.channel!=None:
-                sendch=member.guild.system_channel if self.logch is None else self.logch
+                sendch=member.guild.system_channel
                 if len(after.channel.members)==1:
-                    self.times=datetime.datetime.now().replace(microsecond=0)
                     data={
                         "title":"Initiating call",
                         "fields":[
