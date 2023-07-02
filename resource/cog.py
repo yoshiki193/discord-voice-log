fbc674437bc28462c60f5f9d4ba2f4bea17418b7
maromu193 <maromu193@gmail.com>
2023-07-02 23:52:10 +0900

    "master"

--- resource/cog.py
+++ resource/cog.py
@@ -25,68 +25,75 @@ class command(commands.Cog):
             if not [ i for i in cursor.fetchall() if "vlog" in i]:
                 cursor.execute("CREATE TABLE vlog (guild_name varchar(255),guild_id bigint,ch_id bigint,message_id bigint,unix bigint)")
                 self.psql.commit()
+
+    def judge(self,vs:discord.VoiceState):
+        if vs.channel!=None:
+            er=[i for i in vs.channel.changed_roles if "@everyone" == i.name]
+            if len(er)==1 and vs.channel.overwrites[er[0]].is_empty() or len(er)==0:
+                return 1
     
     @commands.Cog.listener()
     async def on_voice_state_update(self,member:discord.Member,before:discord.VoiceState,after:discord.VoiceState):
-        if after.channel!=before.channel:
-            if before.channel!=None and len(before.channel.members)==0:
-                sendch=member.guild.system_channel
-                with self.psql.cursor() as cursor:
-                    sql=f"SELECT unix, message_id FROM {TABLENAME} WHERE guild_id = %s AND ch_id = %s"
-                    cursor.execute(sql,(f"{member.guild.id}",f"{before.channel.id}"))
-                    tmp=cursor.fetchone()
-                if tmp is not None:
-                    retime=int(time.time())-tmp[0]
-                    delme=await sendch.fetch_message(tmp[1])
-                    await delme.delete()
+        if before.channel!=None and len(before.channel.members)==0 and self.judge(before):
+            sendch=member.guild.system_channel
+            with self.psql.cursor() as cursor:
+                sql=f"SELECT unix, message_id FROM {TABLENAME} WHERE guild_id = %s AND ch_id = %s"
+                cursor.execute(sql,(f"{member.guild.id}",f"{before.channel.id}"))
+                tmp=cursor.fetchone()
+            if tmp is not None:
+                retime=int(time.time())-tmp[0]
+                delme=await sendch.fetch_message(tmp[1])
+                await delme.delete()
+            else:
+                retime=0
+            data={
+                "title":f"{before.channel}",
+                "color":14423100,
+                "fields":[
+                    {
+                        "name":"Time",
+                        "value":f"{datetime.timedelta(seconds=retime)}",
+                        "inline":True
+                    },
+                    {
+                        "name":"By",
+                        "value":f"{member}",
+                        "inline":True
+                    }
+                ]
+            }
+            await sendch.send(embed=discord.Embed.from_dict(data=data))
+    
+        if after.channel!=None and len(after.channel.members)==1 and self.judge(after):
+            sendch=member.guild.system_channel
+            unix=int(time.time())
+            data={
+                "title":f"通話が開始されました <t:{unix}:R>",
+                "color":3978097,
+                "fields":[
+                    {
+                        "name":"Channel",
+                        "value":f"{after.channel}",
+                        "inline":True
+                    },
+                    {
+                        "name":"By",
+                        "value":f"{member}",
+                        "inline":True
+                    }
+                ]
+            }
+            message=await sendch.send(content="@everyone",embed=discord.Embed.from_dict(data=data))
+            with self.psql.cursor() as cursor:
+                ssql=f"SELECT unix FROM {TABLENAME} WHERE guild_id = %s AND ch_id = %s"
+                cursor.execute(ssql,(f"{member.guild.id}",f"{after.channel.id}"))
+                if cursor.fetchone() is None:
+                    sql=f"INSERT INTO {TABLENAME} (guild_name, guild_id, ch_id, message_id, unix) VALUES (%s, %s, %s, %s, %s)"
+                    cursor.execute(sql,(f"{member.guild.name}",f"{member.guild.id}",f"{after.channel.id}", f"{message.id}", f"{unix}"))
                 else:
-                    retime=0
-                data={
-                    "title":f"{before.channel}",
-                    "fields":[
-                        {
-                            "name":"Time",
-                            "value":f"{datetime.timedelta(seconds=retime)}",
-                            "inline":True
-                        },
-                        {
-                            "name":"By",
-                            "value":f"{member}",
-                            "inline":True
-                        }
-                    ]
-                }
-                await sendch.send(embed=discord.Embed.from_dict(data=data))
-        
-            if after.channel!=None and len(after.channel.members)==1:
-                sendch=member.guild.system_channel
-                unix=int(time.time())
-                data={
-                    "title":f"通話が開始されました <t:{unix}:R>",
-                    "fields":[
-                        {
-                            "name":"Channel",
-                            "value":f"{after.channel}",
-                            "inline":True
-                        },
-                        {
-                            "name":"By",
-                            "value":f"{member}",
-                            "inline":True
-                        }
-                    ]
-                }
-                message=await sendch.send(content="@everyone",embed=discord.Embed.from_dict(data=data))
-                with self.psql.cursor() as cursor:
-                    ssql=f"SELECT unix FROM {TABLENAME} WHERE guild_id = %s AND ch_id = %s"
-                    cursor.execute(ssql,(f"{member.guild.id}",f"{after.channel.id}"))
-                    if cursor.fetchone() is None:
-                        sql=f"INSERT INTO {TABLENAME} (guild_name, guild_id, ch_id, message_id, unix) VALUES (%s, %s, %s, %s, %s)"
-                        cursor.execute(sql,(f"{member.guild.name}",f"{member.guild.id}",f"{after.channel.id}", f"{message.id}", f"{unix}"))
-                    else:
-                        sql=f"UPDATE {TABLENAME} SET message_id = %s, unix = %s WHERE guild_id = %s AND ch_id = %s"
-                        cursor.execute(sql,(f"{message.id}", f"{unix}", f"{member.guild.id}", f"{after.channel.id}"))
-                self.psql.commit()
+                    sql=f"UPDATE {TABLENAME} SET message_id = %s, unix = %s WHERE guild_id = %s AND ch_id = %s"
+                    cursor.execute(sql,(f"{message.id}", f"{unix}", f"{member.guild.id}", f"{after.channel.id}"))
+            self.psql.commit()
 
 async def setup(bot:commands.Bot):
     await bot.add_cog(command(bot))
