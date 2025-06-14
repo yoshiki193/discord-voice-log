49e6f51931f801fd7396c1b8927771fed910b129
yoshiki193 <maromu193@gmail.com>
2025-06-14 14:09:08 +0900

    master

--- resource/db.py
+++ resource/db.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-engine = create_engine('sqlite:///data')
+engine = create_engine('sqlite:///opt/data')
 
 Base = declarative_base()
 
